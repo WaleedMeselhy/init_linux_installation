b05bc64b8ac3b0a54e779395e7c8abe7c463e786
waled <waled@localhost.localdomain>
2020-04-11 19:03:03 +0200

    change to fedora

--- init.sh
+++ init.sh
@@ -1,27 +1,63 @@
+#! /bin/bash
 set -e
-sudo apt-get install -y terminator \
-                    zsh \
-                    fonts-powerline
-
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
+USER="waled"
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf check-update
 
+# utilities
+sudo dnf install -y arandr ffmpeg
+
+#fonts
+sudo dnf install -y fira-code-fonts fontawesome-fonts
+
+# docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
+sudo dnf install docker-compose
+
+
+
+docker docker-compose
+git-cola
+htop
+keepass
+meld
+nnn
+okular
+pcmanfm
+peek
+simplenote
+snapd
+teamviewer
+
+the_silver_searcher
+torbrowser-launcher-
+vlc
+code
+zsh \
+    fonts-powerline
 
 chsh waleed -s $(which zsh)
 
-sudo -i -u waleed zsh << EOF
+sudo -i -u waleed zsh <<EOF
 rm -rf  ~/.oh-my-zsh
 zsh  $(wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O -) --unattended
 EOF
-sudo -i -u waleed zsh << EOF
+sudo -i -u waleed zsh <<EOF
 if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]
 then
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 EOF
 
-sudo -i -u waleed zsh << EOF
+sudo -i -u waleed zsh <<EOF
 if [ ! -d ~/.fzf ]
 then
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
@@ -31,7 +67,6 @@ EOF
 sudo cp $ABSDIR/shopt /usr/bin/shopt
 sudo chmod +x /usr/bin/shopt
 
-sudo -i -u waleed bash << EOF
+sudo -i -u waleed bash <<EOF
 cp $ABSDIR/.zshrc ~/.zshrc
 EOF
-
